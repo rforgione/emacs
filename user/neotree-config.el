1023c62277739020af6bb21a8ca07b786284a185
rforgione <rforgione@etsy.com>
2017-05-02 13:19:46 -0400

    add neotree config

---
+++ user/neotree-config.el
@@ -0,0 +1,7 @@
+(setq neo-smart-open t)
+(setq projectile-switch-project-action 'neotree-projectile-action)
+
+(evil-define-key 'normal neotree-mode-map (kbd "TAB") 'neotree-enter)
+(evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
+(evil-define-key 'normal neotree-mode-map (kbd "N") 'neotree-hide)
+(evil-define-key 'normal neotree-mode-map (kbd "N") 'neotree-show) 
